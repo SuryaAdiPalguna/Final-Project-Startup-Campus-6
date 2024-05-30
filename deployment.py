944502ecb1180ac9854aca86f16c8bdb956e4142
SuryaAdiPalguna <suryaadipalguna@gmail.com>
2024-05-30 08:53:12 +0800

    first commit

---
+++ deployment.py
@@ -0,0 +1,17 @@
+import streamlit as st, pandas as pd
+import modelling
+
+df_preprocessed = (pd.read_csv("dataset-5-preprocessed.csv"))
+df = (df_preprocessed[["nama_toko", "unit", "nama", "average_harga_produk_per_unit", "kategori"]].drop_duplicates(ignore_index=True))
+
+st.write("# sistem rekomendasi berdasarkan Harga")
+
+kategori = list(df["kategori"].unique())
+pilihan_kategori = st.selectbox("Masukkan Pilihan Kategori: ", kategori)
+supermarket = list(df["nama_toko"].unique())
+pilihan_supermarket = st.selectbox("Masukkan Pilihan Supermarket: ", supermarket)
+
+tombol = st.button("Lanjut")
+if tombol:
+    already_rated, predictions = modelling.main(df, pilihan_kategori, pilihan_supermarket)
+    st.write(predictions)
